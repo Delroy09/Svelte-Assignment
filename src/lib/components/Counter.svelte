d321610512d23960b377c5aeaa8be2349b0cdcdd
Delroy09 <delroypires@gmail.com>
2026-03-17 16:43:28 +0530

    Add Counter and render on homepage

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,30 @@
+<script>
+  let count = 0;
+  const increment = () => (count += 1);
+  const decrement = () => (count -= 1);
+
+  const reset = () => (count = 0);
+</script>
+
+<div class="counter">
+  <button class="action-btn" onclick={increment}>Plus</button>
+  <span class="value">{count}</span>
+  <button class="action-btn" onclick={decrement}>Minus</button>
+</div>
+
+<style>
+  .action-btn {
+    background-color: rgb(49, 119, 163);
+    color: aliceblue;
+    border-radius: 12px;
+    padding: 12px;
+  }
+  :hover {
+    cursor: pointer;
+  }
+
+  .value {
+    color: rgb(94, 66, 29);
+    font-size: x-large;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Counter from "$lib/components/Counter.svelte";
+  import Hello from "$lib/components/Hello.svelte";
+</script>
+
+<h1>Delroy's Home!</h1>
+<p>Welcome, Human</p>
+
+<main>
+  <Hello name="Sir" />
+  <Counter />
+</main>
+
+<style>
+  * {
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+  }
+</style>
